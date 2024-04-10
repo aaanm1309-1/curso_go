0412b39ae113257b3826d66f8cd7cee304699bd7
Adriano Menezes <72708847+aaanm1309-1@users.noreply.github.com>
2024-04-10 17:40:49 -0300

    Context

---
+++ Context/2-Client-Server/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8083", nil)
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log.Println("Request iniciada")
+	defer log.Println("Request finalizada")
+	select {
+	case <-time.After(4 * time.Second):
+		log.Println("Request processada com sucesso")
+		w.Write([]byte("Request processada com sucesso"))
+	case <-ctx.Done():
+		log.Println("Request cancelada pelo cliente")
+		// http.Error(w, "Request cancelada pelo cliente", http.StatusRequestTimeout)
+	}
+}
