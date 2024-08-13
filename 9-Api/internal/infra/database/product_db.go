9d1a1c11e776398f48c69db6157bba12d1198394
Adriano Menezes <aaanm1309-1@gmail.com>
2024-08-13 00:33:15 -0300

    test

--- 9-Api/internal/infra/database/product_db.go
+++ 9-Api/internal/infra/database/product_db.go
@@ -30,10 +30,15 @@ func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error
 	if sort != "" && sort != "asc" && sort != "desc" {
 		sort = "asc"
 	}
+	// if page != 0 && page > 0 && limit != 0 && limit > 0 {
+	// 	err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("created_at " + sort).Find(&products).Error
+	// } else {
+	// 	err = p.DB.Order("created_at " + sort).Find(&products).Error
+	// }
 	if page != 0 && page > 0 && limit != 0 && limit > 0 {
-		err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("created_at " + sort).Find(&products).Error
+		err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("name " + sort).Find(&products).Error
 	} else {
-		err = p.DB.Order("created_at " + sort).Find(&products).Error
+		err = p.DB.Order("name " + sort).Find(&products).Error
 	}
 	return products, err
 }
--- 9-Api/internal/infra/database/product_db_test.go
+++ 9-Api/internal/infra/database/product_db_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"br.com.adrianomenezes/cursogo/9-Api/internal/entity"
+	ent "br.com.adrianomenezes/cursogo/9-Api/pkg/entity"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -46,7 +47,7 @@ func TestCreateNewProduct(t *testing.T) {
 
 func generateAndSaveProducts(t *testing.T, db *gorm.DB) {
 	for i := 1; i < 45; i++ {
-		product, err := entity.NewProduct(fmt.Sprintf("Product %d", i), rand.Float64()*100)
+		product, err := entity.NewProduct(fmt.Sprintf("Product %02d", i), rand.Float64()*100)
 		assert.NoError(t, err)
 		db.Create(product)
 	}
@@ -59,7 +60,7 @@ func TestFindAllProducts(t *testing.T) {
 
 	products, err := productDb.FindAll(1, 10, "asc")
 	assert.NoError(t, err)
-	assert.Equal(t, products[0].Name, "Product 1")
+	assert.Equal(t, products[0].Name, "Product 01")
 	assert.Equal(t, products[9].Name, "Product 10")
 
 	products, err = productDb.FindAll(2, 10, "asc")
@@ -90,4 +91,89 @@ func TestFindById(t *testing.T) {
 	assert.NotNil(t, product.Name)
 	assert.NotNil(t, product.Price)
 	assert.Greater(t, product.Price, 0.0)
+}
+
+func TestUpdateProduct(t *testing.T) {
+	product, _, productDb := CreateOneProduct(t)
+
+	product.Name = "Teste de Update"
+	product.Price = 233.45
+	errSave := productDb.Update(product)
+
+	assert.Nil(t, errSave)
+
+	productFound, err := productDb.FindByID(product.ID.String())
+
+	assert.Nil(t, err)
+	assert.Equal(t, product.ID, productFound.ID)
+	assert.Equal(t, product.Name, productFound.Name)
+	assert.Equal(t, product.Price, productFound.Price)
+	assert.NotNil(t, product.ID)
+	assert.NotNil(t, product.Name)
+	assert.NotNil(t, product.Price)
+	assert.Greater(t, product.Price, 0.0)
+}
+
+func TestFindByIdWhenRecordNotFound(t *testing.T) {
+	_, productDb := OpenConnection(t)
+
+	_, err := productDb.FindByID(ent.NewID().String())
+	// fmt.Printf("Error: %s", err)
+	assert.Equal(t, err.Error(), "record not found")
+
+}
+
+func TestDeleteProductThatExists(t *testing.T) {
+	product, db, productDb := CreateOneProduct(t)
+
+	err := productDb.Delete(product.ID.String())
+	assert.Nil(t, err)
+
+	productFound, err := productDb.FindByID(product.ID.String())
+	assert.Equal(t, gorm.ErrRecordNotFound, err)
+	assert.Empty(t, productFound)
+
+	db.Migrator().DropTable(&entity.Product{})
+}
+
+func TestDeleteProductThatDoesNotExist(t *testing.T) {
+	_, productDb := OpenConnection(t)
+
+	nonExistentID := ent.NewID().String()
+	err := productDb.Delete(nonExistentID)
+
+	assert.Equal(t, err.Error(), "record not found")
+}
+
+func TestDeleteProductWithNegativeID(t *testing.T) {
+	db, productDb := OpenConnection(t)
+
+	negativeID := "-1"
+	err := productDb.Delete(negativeID)
+
+	assert.Equal(t, err.Error(), "record not found")
+
+	db.Migrator().DropTable(&entity.Product{})
+}
+
+func TestDeleteProductWithEmptyID(t *testing.T) {
+	db, productDb := OpenConnection(t)
+
+	emptyID := ""
+	err := productDb.Delete(emptyID)
+
+	assert.Equal(t, err.Error(), "record not found")
+
+	db.Migrator().DropTable(&entity.Product{})
+}
+
+func TestDeleteProductWithNullID(t *testing.T) {
+	db, productDb := OpenConnection(t)
+
+	nullID := ""
+	err := productDb.Delete(nullID)
+
+	assert.Equal(t, err.Error(), "record not found")
+
+	db.Migrator().DropTable(&entity.Product{})
 }
